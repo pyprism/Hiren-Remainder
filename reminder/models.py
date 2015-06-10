334750f3e996158a924fc09f6bb6985cfe1261af
Ashutosh Das <git.pyprism@gmail.com>
2015-06-10 20:03:47 +0600

    added models

--- reminder/models.py
+++ reminder/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Reminder(models.Model):
+    text = models.TextField(blank=False, null=False)
+    created = models.DateTimeField(auto_now_add=True)
+    reminder = models.DateTimeField()
+    active = models.BooleanField(default=True)
