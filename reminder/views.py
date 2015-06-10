beebb81074436fd821b9c7d1b17d91da5a6d1244
Ashutosh Das <git.pyprism@gmail.com>
2015-06-10 20:03:58 +0600

    added view

--- reminder/views.py
+++ reminder/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from rest_framework import routers, serializers, viewsets
+from django.contrib.auth.models import User
+from .serializers import UserSerializer, ReminderSerializer
+from .models import Reminder
 # Create your views here.
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class ReminderViewSet(viewsets.ModelViewSet):
+    queryset = Reminder.objects.all()
+    serializer_class = ReminderSerializer
