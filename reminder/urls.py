824ea0599d79867a4721745d5215a3d6e2e72a43
Ashutosh Das <git.pyprism@gmail.com>
2015-06-13 22:24:34 +0600

    shit happens

--- reminder/urls.py
+++ reminder/urls.py
@@ -1,15 +1,20 @@
 __author__ = 'prism'
 from django.conf.urls import url, include
 from rest_framework import routers
-from .views import UserViewSet, ReminderViewSet
+from .views import UserViewSet, Reminder, ReminderAll
+from rest_framework.urlpatterns import format_suffix_patterns
 
 
-router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
-router.register(r'reminder', ReminderViewSet)
+#router = routers.DefaultRouter()
+#router.register(r'users', UserViewSet)
+#router.register(r'reminder', Reminder)
 
 urlpatterns = [
-    url(r'^', include(router.urls)),
+    #url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^api-token-auth/', 'rest_framework_jwt.views.obtain_jwt_token'),
+    url(r'^reminder$', ReminderAll.as_view()),
+    url(r'^reminder/(?P<pk>[0-9]+)/$', Reminder.as_view())
 ]
+
+#urlpatterns = format_suffix_patterns(urlpatterns)
