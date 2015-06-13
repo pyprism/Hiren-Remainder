5f19d14d0c19e8833907367f0dc36aadf3437295
Ashutosh Das <git.pyprism@gmail.com>
2015-06-13 22:24:18 +0600

    shit happens

--- reminder/views.py
+++ reminder/views.py
@@ -3,6 +3,11 @@ from rest_framework import routers, serializers, viewsets
 from django.contrib.auth.models import User
 from .serializers import UserSerializer, ReminderSerializer
 from .models import Reminder
+from rest_framework import generics
+from rest_framework.views import APIView
+from django.http import Http404
+from rest_framework.response import Response
+from rest_framework import status
 # Create your views here.
 
 
@@ -11,9 +16,34 @@ class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
 
 
-class ReminderViewSet(viewsets.ModelViewSet):
-    queryset = Reminder.objects.all()
-    serializer_class = ReminderSerializer
+class ReminderAll(APIView):
+
+    def get(self, request):
+        reminders = Reminder.objects.all()
+        serializer = ReminderSerializer(reminders, Many)
+        return Response(serializer.data)
+
+
+class Reminder(APIView):
+
+    def get_object(self, pk):
+        try:
+            return Reminder.objects.get(pk=pk)
+        except Reminder.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        reminder = self.get_object(pk)
+        serializer_class = ReminderSerializer(reminder)
+        return Response(serializer_class.data)
+
+    def put(self, request, pk, format=None):
+        reminder = self.get_object(pk)
+        serializer = ReminderSerializer(reminder, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class Test(viewsets.ModelViewSet):
     pass
