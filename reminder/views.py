d6c72570edcb8ac992b2d5423a1291b1d57ecb71
Ashutosh Das <git.pyprism@gmail.com>
2015-06-16 20:22:36 +0600

    removed unused imports

--- reminder/views.py
+++ reminder/views.py
@@ -1,13 +1,8 @@
-from django.shortcuts import render
-from rest_framework import routers, serializers, viewsets
+from rest_framework import viewsets
 from django.contrib.auth.models import User
 from .serializers import UserSerializer, ReminderSerializer
 from .models import Reminder
 from rest_framework import generics
-from rest_framework.views import APIView
-from django.http import Http404
-from rest_framework.response import Response
-from rest_framework import status
 # Create your views here.
 
 
