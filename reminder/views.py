b701594834f776f7487d012b91e94f3daf02296d
Ashutosh Das <git.pyprism@gmail.com>
2017-05-04 20:18:12 +0600

    added login view

--- reminder/views.py
+++ reminder/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.contrib import auth
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+
+def index(request):
+    if request.user.is_authenticated:
+        return redirect('recipes')
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = auth.authenticate(username=username, password=password)
+        if user:
+            auth.login(request, user)
+            return redirect('recipes')
+        else:
+            messages.error(request, 'Username/Password is not valid!')
+            return redirect('/')
+    else:
+        return render(request, 'login.html')
