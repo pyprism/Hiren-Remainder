93aaaac55424bfc2a2597da5d01a143ac2a0a306
Ashutosh Das <git.pyprism@gmail.com>
2017-05-06 19:27:30 +0600

    wip

--- reminder/views.py
+++ reminder/views.py
@@ -7,16 +7,21 @@ from django.shortcuts import get_object_or_404
 
 def index(request):
     if request.user.is_authenticated:
-        return redirect('recipes')
+        return redirect('create')
     if request.method == "POST":
         username = request.POST.get('username')
         password = request.POST.get('password')
         user = auth.authenticate(username=username, password=password)
         if user:
             auth.login(request, user)
-            return redirect('recipes')
+            return redirect('create')
         else:
             messages.error(request, 'Username/Password is not valid!')
             return redirect('/')
     else:
         return render(request, 'login.html')
+
+
+@login_required
+def add(request):
+    return render(request, 'add.html', {'title': 'Add New Reminder'})
