3650f6ad4b1f80782b826230d6f33662862a93d1
Ashutosh Das <git.pyprism@gmail.com>
2017-05-10 11:36:01 +0600

    Added new views

--- reminder/views.py
+++ reminder/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.contrib import auth
+from .forms import ReminderForm, ProfileForm
+from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
+import logging
 
 
 def index(request):
@@ -24,4 +27,17 @@ def index(request):
 
 @login_required
 def add(request):
+    if request.method == 'POST':
+        reminder_form = ReminderForm(request.POST)
+        if reminder_form.is_valid():
+            form = reminder_form.save(commit=False)
+            user = User.objects.get(username=request.user.username)
+            form.user = user
+            form.save()
+            messages.info(request, 'New Reminder Added')
+        else:
+            logger = logging.getLogger(__name__)
+            messages.error(request, reminder_form.errors)
+            logger.info(reminder_form.errors)
+        return redirect('create')
     return render(request, 'add.html', {'title': 'Add New Reminder'})
