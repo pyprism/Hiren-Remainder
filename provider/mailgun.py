d9079de0d4cc6c5fdfd4473045a631d10273973f
Ashutosh Das <git.pyprism@gmail.com>
2017-05-29 14:55:56 +0600

    added mailgun api

--- provider/mailgun.py
+++ provider/mailgun.py
@@ -8,4 +8,25 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hiren.settings")
 django.setup()
 
 
-def mail()
+def mail(url, key, sender, recipient, title, message):
+    """
+    Mailgun sender
+    :param url: mailgun api url
+    :param key: mailgun api key
+    :param sender: sender email
+    :param recipient: recipient email
+    :param title: email title
+    :param message: email body
+    :return:
+    """
+    logger = logging.getLogger(__name__)
+    try:
+        requests.post(url + '/messages', auth=('api', key),
+                      data={
+                          "from": "New Reminder <" + sender + ">",
+                          "to": [recipient],
+                          "subject": "Reminder about: " + title,
+                          "text": message
+                      })
+    except Exception as e:
+        logger.error(e)
