6407a648382ad73980ed8af3abcaf48193beec04
Ashutosh Das <git.pyprism@gmail.com>
2017-05-29 15:50:07 +0600

    Added twillo

--- provider/twillo.py
+++ provider/twillo.py
@@ -1,6 +1,24 @@
 from twilio.rest import Client
+import django
+import os
+import logging
 
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hiren.settings")
+django.setup()
+
+
+def sms(sid, token, to, from_, body):
+    try:
+        client = Client(sid, token)
+        client.messages.create(
+            to=to,
+            from_=from_,
+            body=body
+        )
+    except Exception as e:
+        logger = logging.getLogger(__name__)
+        logger.error(e)
 
 # Your Account SID from twilio.com/console
 account_sid = ""
