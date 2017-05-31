cc10b51104190d6e4679b68aafa2881f7e1aac3e
Ashutosh Das <git.pyprism@gmail.com>
2017-05-31 13:14:14 +0600

    added model test

--- reminder/tests.py
+++ reminder/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+from django.urls import resolve
+from django.test import TestCase, TransactionTestCase
+from django.contrib.auth.models import User
+from django.test import Client
+from . import views
+from .models import Profile, Reminder
+from freezegun import freeze_time
+from django.utils import timezone
 
-# Create your tests here.
+
+class ModelTest(TransactionTestCase):
+    reset_sequences = True
+
+    @freeze_time("2012-01-14")
+    def setUp(self):
+        self.user = User.objects.create_user('hiren', 'a@b.com', 'bunny')
+        Reminder.objects.create(user=self.user, date_time=timezone.now(), title="hello", text="hiren", email=True)
+
+    def test_reminder_model(self):
+        count = Reminder.objects.count()
+        self.assertEqual(count, 1)
+
+        reminder = Reminder.objects.get(pk=1)
+        self.assertEqual(reminder.title, "hello")
