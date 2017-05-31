515faaa3ec14c87fc906fbef06f458025f23393f
Ashutosh Das <git.pyprism@gmail.com>
2017-05-31 13:20:46 +0600

    added index view test

--- reminder/tests.py
+++ reminder/tests.py
@@ -22,3 +22,33 @@ class ModelTest(TransactionTestCase):
 
         reminder = Reminder.objects.get(pk=1)
         self.assertEqual(reminder.title, "hello")
+
+
+class LoginViewTest(TestCase):
+    """
+    Test for authentication
+    """
+
+    def setUp(self):
+        User.objects.create_user('hiren', 'a@b.com', 'password')
+        self.c = Client()
+
+    def test_login_url_resolves_to_login_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, views.index)
+
+    def test_auth_works(self):
+        respond = self.c.post('/', {'username': 'hiren', 'password': 'password'})
+        self.assertRedirects(respond, '/reminders/')
+
+    def test_redirect_for_unauthenticated_user_works(self):
+        response = self.c.get('/reminders/')
+        self.assertRedirects(response, '/?next=/reminders/')
+
+    def test_redirect_works_for_bad_auth(self):
+        respond = self.c.post('/', {'username': 'hiren', 'password': 'bad pass'})
+        self.assertRedirects(respond, '/')
+
+    def test_view_returns_correct_template(self):
+        response = self.c.get('/')
+        self.assertTemplateUsed(response, 'login.html')
