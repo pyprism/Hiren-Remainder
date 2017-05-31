f647b1a2ff497c502bce3c84f22770f5ffed653f
Ashutosh Das <git.pyprism@gmail.com>
2017-05-31 12:55:16 +0600

    removed unused codes

--- provider/twillo.py
+++ provider/twillo.py
@@ -19,17 +19,3 @@ def sms(sid, token, to, from_, body):
     except Exception as e:
         logger = logging.getLogger(__name__)
         logger.error(e)
-
-# Your Account SID from twilio.com/console
-account_sid = ""
-# Your Auth Token from twilio.com/console
-auth_token = "your_auth_token"
-
-client = Client(account_sid, auth_token)
-
-message = client.messages.create(
-    to="+15558675309",
-    from_="+15017250604",
-    body="Hello from Python!")
-
-print(message.sid)
