f735a78457db370661c580b9acaaa76fd450c645
Ashutosh Das <git.pyprism@gmail.com>
2017-06-26 11:33:06 +0600

    fixed token response

--- api/views.py
+++ api/views.py
@@ -3,15 +3,22 @@ from django.contrib.auth.models import User
 from rest_framework.authtoken.models import Token
 from django.contrib import auth
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 
+@csrf_exempt
 def login(request):
+    """
+    token authentication for api
+    :param request:
+    :return:
+    """
     if request.method == "POST":
         username = request.POST.get('username')
         password = request.POST.get('password')
         user = auth.authenticate(username=username, password=password)
         if user:
             token = Token.objects.get_or_create(user=user)
-            return JsonResponse({'token': token.key})
+            return JsonResponse({'token': str(token[0])})
         else:
             return JsonResponse({'error': 'Username/Password is not valid'})
