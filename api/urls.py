e10e7f52815a644d89de70840032867195f07978
Ashutosh Das <git.pyprism@gmail.com>
2017-10-21 17:22:04 +0600

    added new route

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,14 @@
 from django.conf.urls import url, include
 from api import views
+from rest_framework import routers
+from .views import ReminderViewSet
+
+
+router = routers.DefaultRouter()
+router.register(r'reminder', ReminderViewSet)
 
 urlpatterns = [
-    url(r'auth/', views.login),
+    url(r'token/', views.login),
+    url(r'^reminder/', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
