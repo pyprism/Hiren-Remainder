35dcfa729a96b215841b2eb54b326543bf41c44b
Ashutosh Das <git.pyprism@gmail.com>
2017-10-21 17:21:24 +0600

    Added new test

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
-
+from django.test import TestCase, TransactionTestCase
+from rest_framework.test import APIRequestFactory, APIClient
+from freezegun import freeze_time
+from django.contrib.auth.models import User
 # Create your tests here.
+
+
+class TestToken(TestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+        User.objects.create_user('hiren', 'a@b.com', 'password')
+
+    def test_auth_token(self):
+        response = self.client.post('/api/token/', {'username': 'hiren', 'password': 'password'})
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('token' in response.json())
+
+
+
+
