c955b2e1aacb6599586e1f1149cd8467bf386957
Ashutosh Das <git.pyprism@gmail.com>
2017-12-26 18:19:30 +0600

    added name in test

--- api/tests.py
+++ api/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase, TransactionTestCase
+from django.shortcuts import reverse
 from rest_framework.test import APIRequestFactory, APIClient
 from freezegun import freeze_time
 from django.contrib.auth.models import User
@@ -12,14 +13,17 @@ class TestToken(TestCase):
         User.objects.create_user('hiren', 'a@b.com', 'password')
 
     def test_auth_token(self):
-        response = self.client.post('/api/token/', {'username': 'hiren', 'password': 'password'})
+        response = self.client.post(reverse("token"), {'username': 'hiren', 'password': 'password'})
         self.assertEqual(response.status_code, 200)
         self.assertTrue('token' in response.json())
 
     def test_error(self):
-        response = self.client.post('/api/token/', {'username': 'hiren', 'password': 'bad password'})
+        response = self.client.post(reverse("token"), {'username': 'hiren', 'password': 'bad password'})
         self.assertEqual(response.status_code, 401)
         self.assertTrue('error' in response.json())
 
+
+class ReminderViewTest(TransactionTestCase):
+    reset_sequences = True
 
 
